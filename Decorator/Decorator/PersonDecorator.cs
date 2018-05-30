14109b8a44c7990bad42c474a964bd80f28f421c
senner007 <nielshtg@hotmail.com>
2018-05-30 21:45:47 +0200

    add C# Decorator Pattern

---
+++ Decorator/Decorator/PersonDecorator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    public class PersonDecorator : IPerson
+    {
+        
+        IPerson person;
+
+        public PersonDecorator(IPerson _person)
+        {
+            person = _person;
+            Description = person.Description;
+            Name = person.Name;
+        }
+
+        public string Description { get; set; }
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return Name + ", " + Description;
+        }
+    }
+    public class HackingSkills : PersonDecorator
+    {
+        public HackingSkills(IPerson _person) : base(_person)
+        {
+            Description = _person.Description + ", has Hacking Skills";
+        }
+    }
+    public class FlyingSkills : PersonDecorator
+    {
+        public FlyingSkills(IPerson _person) : base(_person)
+        {
+            Description = _person.Description + ", has Flying Skills";
+        }
+    }
+}
---
+++ Decorator/Decorator/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IPerson h = new Hacker("Neo");
+
+            h = new HackingSkills(h);
+            h = new FlyingSkills(h);
+            Console.WriteLine(h);
+
+
+            IPerson s = new Student("John");
+
+            s = new HackingSkills(s);
+            s = new FlyingSkills(s);
+            Console.WriteLine(s);
+        }
+    }
+    public interface IPerson
+    {
+        string Description { get; set; }
+        string Name { get; set; }
+    }
+    public abstract class Person : IPerson
+    {
+        public string Description { get; set; }
+        public string Name { get; set; }
+        public Person(string name, string description)
+        {
+            Name = name;
+            Description = "is a " + description;
+        }
+    }
+
+    public class Student : Person
+    {
+        public Student(string name, string description = "Student") : base(name, description)
+        {
+        }
+    }
+
+    public class Employee : Person
+    {
+        public Employee(string name, string description = "Employee") : base(name, description)
+        {
+        }
+    }
+    public class Hacker : Person
+    {
+        public Hacker(string name, string description = "Hacker") : base(name, description)
+        {
+        }
+    }
+}
